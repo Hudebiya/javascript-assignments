578ebf1493db38ae04aeb3d7305ce9f2aa4240c0
Hudebiya Bano <HudebiyaBano@gmail.com>
2025-09-25 09:45:16 -0700

    initial

---
+++ chapter 35to38/app.js
@@ -0,0 +1,6 @@
+               //     CHAPTER  35 To 38
+
+// Qno:1. Write a function that displays current date & time in your browser.
+
+
+// Qno:2. Write a function that takes first & last name and then it greets the user using his full name.
