91aea3fd89ee4f6fde81e2ba6e9f1304bc7e3419
Hudebiya Bano <HudebiyaBano@gmail.com>
2025-10-01 09:25:49 -0700

    initial commit

--- practice work/app.js
+++ practice work/app.js
@@ -1,10 +1,16 @@
 //       PRACTICE WORK
 
-var today=new Date(); 
-var targetDate=new Date("2/18/2026");
-var diff = targetDate - today;
-var oneDay = 1000 * 60 * 60 * 24
-var daysLeft=Math.ceil(diff/ oneDay);
+// var today=new Date(); 
+// var targetDate=new Date("2/18/2026");
+// var diff = targetDate - today;
+// var oneDay = 1000 * 60 * 60 * 24
+// var daysLeft=Math.ceil(diff/ oneDay);
 
-console.log("Ramadan is coming with in" + (daysLeft) + "days");
+// console.log("Ramadan is coming with in" + (daysLeft) + "days");
+
+
+                 //     CALCULATOR EXAMPLE:
+
+
+
 
