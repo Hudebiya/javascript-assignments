9f01204c807bcc5c692e6057c115cb1189b05b1e
Hudebiya Bano <HudebiyaBano@gmail.com>
2025-10-07 09:28:37 -0700

    initial changes

--- Simple Calculator/app.js
+++ Simple Calculator/app.js
@@ -1,8 +1,6 @@
-            //  SIMPLE CALCULATOR
-            
-function put(value) {
+function press(value) {
   var display = document.getElementById("display");
-  display.value = display.value + value; 
+  display.value = display.value + value;
 }
 
 function clearDisplay() {
@@ -10,10 +8,29 @@ function clearDisplay() {
 }
 
 function calculate() {
-  var calculation = document.getElementById("display").value;
-  
-  if (calculation) {
-    var result = eval(calculation);  
-    document.getElementById("display").value = result;
+  var expression = document.getElementById("display").value;
+  var result = 0;
+
+  if (expression.indexOf("+") !== -1) {
+    var parts = expression.split("+");
+    result = Number(parts[0]) + Number(parts[1]);
   }
+  else if (expression.indexOf("-") !== -1) {
+    var parts = expression.split("-");
+    result = Number(parts[0]) - Number(parts[1]);
+  }
+  else if (expression.indexOf("*") !== -1) {
+    var parts = expression.split("*");
+    result = Number(parts[0]) * Number(parts[1]);
+  }
+  else if (expression.indexOf("/") !== -1) {
+    var parts = expression.split("/");
+    result = Number(parts[0]) / Number(parts[1]);
+  }
+  else {
+    document.getElementById("display").value = "Error!";
+    return;
+  }
+
+  document.getElementById("display").value = result;
 }
