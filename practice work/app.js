00643db43eb2876eee5a9c96006adaa4d2d23dbf
Hudebiya Bano <HudebiyaBano@gmail.com>
2025-10-08 05:27:20 -0700

    initial changes

--- practice work/app.js
+++ practice work/app.js
@@ -9,7 +9,29 @@
 // console.log("Ramadan is coming with in" + (daysLeft) + "days");
 
 
-                 //     CALCULATOR EXAMPLE:
+                 //   Dice Rolling Game
+
+// var score = 0;
+
+// for (var i = 1; i <= 5; i++) {
+//     var userNum = +prompt(`Round ${i}: Enter a number between 1 and 6`);
+//     var guessNum = Math.ceil(Math.random() * 6);
+
+//     console.log(`Your number is: ${userNum}`);
+//     console.log(`Computer number is: ${guessNum}`);
+
+//     if (userNum === guessNum) {
+//         console.log(" You Win!");
+//         score++;
+//     } else {
+//         console.log(" Sorry, Try again.");
+//     }
+
+//     console.log("----------");
+// }
+
+// console.log("Game Over! Your total score is: ${score} out of 5");             
+                 
 
 
 
