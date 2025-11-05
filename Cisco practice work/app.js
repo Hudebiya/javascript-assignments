a2e808ca674620ee407b4053761aef808cd983f8
Hudebiya Bano <HudebiyaBano@gmail.com>
2025-11-05 04:13:02 -0800

    Practice

---
+++ Cisco practice work/app.js
@@ -0,0 +1,36 @@
+                //  Cisco Practice Work
+            
+//   1.  ARRAY
+
+// var names = [["Olivia", "Emma", "Mia", "Sofia"], ["William", "James", "Daniel"]];
+// console.log(names[0]); // -> ["Olivia", "Emma", "Mia", "Sofia"]
+// console.log(names[0][1]); // -> Emma
+// console.log(names[1][1]); // -> James
+// var femaleNames = names[0];
+// console.log(femaleNames[0]); // -> Olivia
+// console.log(femaleNames[2]); // -> Mia
+
+// var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var day = "Sunday";
+// console.log(typeof days); // -> object
+// console.log(typeof day); // -> string
+// console.log(days instanceof Array); // -> true
+// console.log(day instanceof Array); // -> false
+
+//   2. PUSH
+
+// var names = ["Olivia", "Emma", "Mateo", "Samuel"];
+// console.log(names.length); // -> 4
+// names.push("Amelia");
+// console.log(names.length); // -> 5
+// console.log(names); // - > ["Olivia", "Emma", "Mateo","Samuel", "Amelia"]
+
+//   3. POP
+
+// var names= ["Olivia", "Emma", "Mateo", "Samuel"];
+// console.log(names.length); // -> 4
+   
+// var name = names.pop();
+// console.log(names.length); // -> 3
+// console.log(name); // -> Samuel
+// console.log(names); // -> ["Olivia", "Emma", "Mateo"]
