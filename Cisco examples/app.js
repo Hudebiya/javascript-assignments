8c1d8f13fad8d1eee1f83aeffd1bd6ca51cc675d
Hudebiya Bano <HudebiyaBano@gmail.com>
2025-11-07 09:38:59 -0800

    continue

--- Cisco examples/app.js
+++ Cisco examples/app.js
@@ -72,5 +72,14 @@
 // console.log(n4); // -> ["Samuel"]
 // console.log(names); // -> ["Olivia", "Emma", "Mateo", "Samuel"]
 
+//   ARITHMETIC  MEAN
 
+/* var x = 5;
+var y = 2;
+console.log("addition: ", x + y); // -> 7
+console.log("subtraction: ", x - y); // -> 3
+console.log("multiplication: ", x * y); // -> 10
+console.log("division: ", x / y); // -> 2.5
+console.log("division remainder :", x % y); // -> 1
+console.log("exponent: ", x ** y); // -> 25 */
 
