078b5657a8de668768315269b188f1b20a59f20a
Hudebiya Bano <HudebiyaBano@gmail.com>
2025-11-13 11:53:35 +0500

    continue

--- practice work/app.js
+++ practice work/app.js
@@ -1,84 +1,27 @@
 //       PRACTICE WORK
 
-// document.querySelectorAll(".close-btn").forEach(btn => {
-//   btn.onclick = function() {
-//     this.parentElement.style.display = "none";
-//   };
-// });
+// alert("Welcome to JavaScript Learning by Hudebiya!");          // alert
+
+/* var userCity = "Karachi";
+var userCountry = "Pakistan";
+var fullLocation = "You live in Karachi, Pakistan.";
+
+alert(userCity);
+alert(userCountry);
+alert(fullLocation);           //  Variables for Strings    */
+
+/* var userGreeting = "Hello";
+var userName = "Hudebiya";
+var userMessage = userGreeting + ", " + userName + "! Welcome to JavaScript.";
+alert(userMessage);
+
+var sentenceStart = "My favorite movie is ";
+var movieName = "Frozen";
+var finalMessage = sentenceStart + movieName + ".";
+alert(finalMessage);                    //  String Concatenation    */
 
 
-// var today=new Date(); 
-// var targetDate=new Date("2/18/2026");
-// var diff = targetDate - today;
-// var oneDay = 1000 * 60 * 60 * 24
-// var daysLeft=Math.ceil(diff/ oneDay);
 
-// console.log("Ramadan is coming with in" + (daysLeft) + "days");
-
-
-                 //   Dice Rolling Game
-
-// var score = 0;
-
-// for (var i = 1; i <= 5; i++) {
-//     var userNum = +prompt(`Round ${i}: Enter a number between 1 and 6`);
-//     var guessNum = Math.ceil(Math.random() * 6);
-
-//     console.log(`Your number is: ${userNum}`);
-//     console.log(`Computer number is: ${guessNum}`);
-
-//     if (userNum === guessNum) {
-//         console.log(" You Win!");
-//         score++;
-//     } else {
-//         console.log(" Sorry, Try again.");
-//     }
-
-//     console.log("----------");
-// }
-
-// console.log("Game Over! Your total score is: ${score} out of 5"); 
-
-// function on(e){
-//     e.src="https://icon2.cleanpng.com/lnd/20250114/hl/4984290bf09cd2619afe1567e202eb.webp"
-
-// }
-                 
-// function off(e){
-//     e.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsl9-lHR-M-BcavZfEe2KN3PR_Vn7FysVy4kDgTOOwS36TgRLjg6s3E4&s"
-    
-// }
-
-//  function checkAddress(fieldId) {
-//     if (document.getElementById(fieldId).value === "") {
-     
-// alert("Email address required.");
-//     }
-//  }
- 
-// var container = document.getElementById("list");
-
-// function showList() {
-//   var list = `
-//     <ul>
-//         <li>Karachi</li>
-//         <li>Hyderabad</li>
-//         <li>Lahore</li>
-//         <li>Islamabad</li>
-//     </ul>
-
-//       <button onclick="hideList()" >Hide List</button>
-
-//      `;
-
-//   container.innerHTML = list;
-// }
-
-// function hideList() {
-//   container.innerHTML = "";
-// }
-
-   
 
 
 
