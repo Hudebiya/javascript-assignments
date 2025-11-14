7375dbeffc0c914ff0c310b8df968ad9ef365ef6
Hudebiya Bano <HudebiyaBano@gmail.com>
2025-11-14 16:44:42 +0500

    continue

--- Cisco examples/app.js
+++ Cisco examples/app.js
@@ -316,6 +316,38 @@ Function factorial(n) {
 console.log(factorial(20)); // -> 2432902008176640000
 console.log(factorial(1000)); // -> Uncaught RangeError: Max value 20  */
 
+/*    Object
+
+console.log(typeof 2.5); // -> number
+console.log(typeof "one two three"); // -> string
+console.log(typeof false); // -> boolean
+
+let nr = 2.5; 
+nr = nr / 2;
+console.log(typeof nr); // -> number
+
+let a = [10, 20, "en to tre", true, 50];
+a[4] = a[4] * 2;
+console.log(a[0]);    // -> 10
+console.log(a[2]);    // -> en to tre
+console.log(a[4]);    // -> 100             Array as acomplex type
+
+let sampleObject = {
+    id: 10, 
+    delay: 20,
+    name: "en to tre",
+    isPresent: true,
+    delay: 50
+};
+sampleObject.delay = sampleObject.delay * 2;
+console.log(sampleObject.id);    // -> 10
+console.log(sampleObject.name);    // -> en to tre
+console.log(sampleObject.delay);    // -> 100    */
+
+
+
+
+
 
 
 
