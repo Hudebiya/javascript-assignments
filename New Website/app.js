7687ae232ad83a36353074a8932700c45ba27d11
Hudebiya Bano <HudebiyaBano@gmail.com>
2025-11-23 18:07:57 +0500

    Continue

--- New Website/app.js
+++ New Website/app.js
@@ -1,149 +1,241 @@
-             //    
-var beautyProruct = {
-    skinCare : {
-       Vaseline : {
-         "Body Lotion" : {
-            Name: "Vaseline Lotion",
-            price: 500,
-            src: "https://greenvalley.pk/cdn/shop/files/Vaseline_Body_Lotion_Total_Moisture_Nourishing_100Ml.webp?v=1754552311",
-            vol: 100,
-            Description:"Vaseline Pro VitaB3 Serum Burst Lotion Luminous Glow Healthy, Glowing Skin, Lightweight 10x Antioxidant Power of Vitamin C with Vitamin B3, Hyaluronic Acid & Vitamin E, 7.5 Fl Oz",
-         },
-         "Lip Balm" : {
-            Name : "Vaseline Lip Balm",
-            price: 500,
-            src: "https://www.catchnpack.pk/wp-content/uploads/2024/12/Vaseline-Lip-Therapy-Creme-Brulee-Mini-7g.jpg",
-            vol: 100,
-            Description:"Vaseline Pro VitaB3 Serum Burst Lotion Luminous Glow Healthy, Glowing Skin, Lightweight 10x Antioxidant Power of Vitamin C with Vitamin B3, Hyaluronic Acid & Vitamin E, 7.5 Fl Oz",
-         },
-         "Healing Jelly" : {},
-       },
-       Nivea : {
-         "Nivea Cream" : {},
+var beautyProducts = {
+  skinCare: {
+    Vaseline: {
+      "Body Lotion": {
+        Name: "Vaseline Lotion",
+        price: 800,
+        src: "https://greenvalley.pk/cdn/shop/files/Vaseline_Body_Lotion_Total_Moisture_Nourishing_100Ml.webp?v=1754552311",
+        vol: 750,
+        Description: "Vaseline Serum Burst Lotion with Vitamin C & E."
+      },
+      "Lip Balm": {
+        Name: "Vaseline Lip Balm",
+        price: 500,
+        src: "https://m.media-amazon.com/images/I/612Lqn-CdUL._AC_UL320_.jpg",
+        vol: 100,
+        Description: "Vaseline Lip Therapy for soft & smooth lips."
+      },
+      "Healing Jelly": {
+        Name: "Vaseline Petroleum Jelly",
+        price: 800,
+        src: "https://m.media-amazon.com/images/I/81F0uAkolUL._AC_UL320_.jpg",
+        vol: 250,
+        Description: "Vaseline Original Healing Jelly moisturizes & heals skin."
+      }
+    },
 
-       },
-       Ponds : {},
-       Olay : {},
-       "Saeed Ghani" : {},
+    Nivea: {
+      "Nivea Cream": {
+        Name: "NIVEA Creme",
+        price: 850,
+        src: "https://m.media-amazon.com/images/I/71IsMWTrn-L._AC_UL320_.jpg",
+        vol: 500,
+        Description: "NIVEA classic moisturizing cream for face & body."
+      },
+      "Moisturizing Creme": {
+        Name: "NIVEA Soft Moisturizing Creme",
+        price: 900,
+        src: "https://m.media-amazon.com/images/I/71gee2raLaL._AC_UL320_.jpg",
+        vol: 100,
+        Description: "NIVEA Soft refreshing & lightweight moisturizer."
+      },
+      "Body Lotion": {
+        Name: "NIVEA Cocoa Butter Body Lotion",
+        price: 1000,
+        src: "https://m.media-amazon.com/images/I/71t+j+fWpjL._SX425_.jpg",
+        vol: 100,
+        Description: "NIVEA Cocoa Butter Lotion for dry skin."
+      }
     },
-    hairCare : {
-       Dove : {},
-       Sunsilk : {},
-       Pantene : {},
-       "L'Oreal Paris" : {},
-       Tresemme : {},
-       "Head & Shoulders" : {},
-    },
-    Makeup : {
-       Maybelline : {},
-       "Huda Beauty" : {},
-       "MAC Cosmetics" : {},
-       NYX : {},
-       Revlon : {},
-    },
-    Fragnance : {
-       "Victoria's Secret" : {},
-       Channel : {},
-       Dior : {},
-       Gucci : {},
-    },
-   }
 
+    Ponds: {
+      "Pond's Face Wash": {
+        Name: "Pond's Beauty Face Wash",
+        price: 600,
+        src: "https://m.media-amazon.com/images/I/51LNBCOt4JL._SX425_.jpg",
+        vol: 100,
+        Description: "Pond's brightening facial cleanser."
+      },
+      "Pond's Cold Cream": {
+        Name: "Pond's Cold Cream Cleanser",
+        price: 750,
+        src: "https://m.media-amazon.com/images/I/61kwaIg6MrL._SX425_.jpg",
+        vol: 500,
+        Description: "Pond's deep cleansing & moisturizing cold cream."
+      },
+      "Pond's Charcoal": {
+        Name: "POND'S Charcoal Face Wash",
+        price: 650,
+        src: "https://m.media-amazon.com/images/I/51AmjNNFiyL._SX425_.jpg",
+        vol: 100,
+        Description: "Charcoal detox wash for removing pollution & dirt."
+      },
+    },
+
+    "Saeed Ghani": {
+      "Saeed Ghani Onion Shampoo": {
+        Name: "Saeed Ghani Onion Shampoo",
+        price: 800,
+        src: "https://m.media-amazon.com/images/I/41fVvu0xtjL._SX425_.jpg",
+        vol: 200,
+        Description: "Saeed Ghani Onion Shampoo is a gentle yet effective hair care solution designed to support your journey to thicker, fuller-looking hair."
+    },
+    "Saeed Ghani Almond Oil": {
+        Name: "Saeed Ghani Almond Oil",
+        price: 500,
+        src: "https://m.media-amazon.com/images/I/5100eJvaefL._SX425_.jpg",
+        vol: 100,
+        Description: "Almond oil is beneficial for skin and hair both. If applied to hair it nourishes hair and smoothes hair cuticles, makes hair long."
+    },
+    "Saeed Ghani Aloevera Gel": {
+        Name: "Saeed Ghani Aloevera Gel",
+        price: 600,
+        src: "https://m.media-amazon.com/images/I/51B2UBBJvnL._SX425_.jpg",
+        vol: 200,
+        Description: "Aloevera Gel is a moisturizing day and night cream especially formulated for aging and sagging skin."
+    },
+   },
+  },
+
+  hairCare: {
+    Dove: {
+      "Dove Body Scrub":{
+         Name: "Dove Body Scrub",
+         price: 800,
+         src: "https://m.media-amazon.com/images/I/71F1CH9-7wL._SX425_.jpg",
+         vol: 250,
+         Description:"Dove Scrubs are created with moisturizing cream to prepare your skin with this unique Himalayan Salt & Rose Oil",
+      },
+      "Dove Conditioner": {
+        Name: "Dove Conditioner Intensive Repair",
+        price: 1000,
+        src:"https://m.media-amazon.com/images/I/71q+A8PqQXL._SX425_.jpg",
+        vol: 500,
+        Description:"Dove Intensive Repair hair conditioner restores with every wash-hair is reborn 10X stronger when using",
+      },
+      "Dove Beauty Bar Soap": {
+        Name: "Dove Beauty Bar Soap Shea Butter",
+        price: 500,
+        src: "https://m.media-amazon.com/images/I/710fpmk09gL._SX425_.jpg",
+        vol: 100,
+        Description:" Dove Shea Butter Beauty Bar with shea & vanilla scent gives your skin a warm lather for a pampering cleanse",
+      },
+    },
+    Sunsilk: {
+    "Sunsilk Shampoo": {
+        Name: "Sunsilk Stunning Black Shine Shampoo",
+        price: 850,
+        src: "https://m.media-amazon.com/images/I/51npbp5MNuS._SX425_.jpg",
+        vol: 500,
+        Description:"Sunsilk Black Shine is enriched with amla pearl complex.It nourishes hair and works from the first wash,to give you shinier hair",
+      },
+    "Sunsilk Smooth Shampoo": {
+        Name: "Sunsilk Nourishing Soft and Smooth Shampoo",
+        price: 850,
+        src: "https://m.media-amazon.com/images/I/41gtAhyLXtS._SX425_.jpg",
+        vol: 500,
+        Description:"Sunsilk nourishing soft and smooth shampoo helps in keeping your hair smooth and strong.",
+      },
+    "Sunsilk Lusciously Shampoo": {
+        Name: "Sunsilk Lusciously Thick and Long Shampoo",
+        price: 850,
+        src: "https://m.media-amazon.com/images/I/41r2k+dWtIS._SX425_.jpg",
+        vol: 500,
+        Description:"Sunsilk Lusciously Thick & Long shampoo provides hair with essential nutrients to give you long-lasting volume",
+      },
+    },
+    Pantene: {},
+  },
+
+  Makeup: {
+    Maybelline: {},
+    "Huda Beauty": {},
+    "MAC Cosmetics": {},
+    NYX: {},
+    Revlon: {}
+  },
+
+  Fragnance: {
+    "Victoria's Secret": {},
+    Channel: {},
+    Dior: {},
+    Gucci: {}
+  }
+};
 
 var productMenu = document.getElementById("productMenu");
-var allProducts = document.getElementById("allProducts");
-for (product in beautyProducts) {
-  console.log(product);
-  productMenu.innerHTML += `
-            <option value="${product}">${product.toUpperCase()}</option>
-
-    `;
-  for (brand in beautyProducts[product]) {
-    // console.log(beautyProducts[product][brand].name);
-
-    allProducts.innerHTML += `
- <div class="col">
-          <div class="card">
-            <img
-              src=${beautyProducts[product][brand].src}
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-title">${beautyProducts[product][brand].name}</h5>
-              <p class="card-text">${beautyProducts[product][brand].description}</p>
-            </div>
-            <div class="card-footer">
-              <h5 class="price">Rs.${beautyProducts[product][brand].price}/- </h5>
-            </div>
-          </div>
-        </div>`;
-  }
-}
 var brandMenu = document.getElementById("brandMenu");
-function productChange() {
-  brandMenu.innerHTML = ` <option selected>Select Brand</option>`;
-  console.log(beautyProducts[productMenu.value]);
-  for (brand in beautyProducts[productMenu.value]) {
-    // console.log(brand);
-    brandMenu.innerHTML += `
-                <option value="${brand}">${brand.toUpperCase()}</option>
+var allProducts = document.getElementById("allProducts");
 
+for (var category in beautyProducts) {
+  productMenu.innerHTML += `
+    <option value="${category}">${category.toUpperCase()}</option>
+  `;
+}
+
+function productChange() {
+  brandMenu.innerHTML = `<option selected>Select Brand</option>`;
+
+  var selectedCategory = productMenu.value;
+
+  for (var brand in beautyProducts[selectedCategory]) {
+    brandMenu.innerHTML += `
+      <option value="${brand}">${brand.toUpperCase()}</option>
     `;
   }
 }
+
 function viewAll() {
-  console.log("all products");
+  allProducts.innerHTML = "";
 
-  //   for(brand in beautyProducts[product]){
-  //         console.log(beautyProducts[product][brand].name);
+  for (var category in beautyProducts) {
+    for (var brand in beautyProducts[category]) {
+      for (var item in beautyProducts[category][brand]) {
+        var p = beautyProducts[category][brand][item];
 
-  // allProducts.innerHTML += `
-  //  <div class="col">
-  //           <div class="card">
-  //             <img
-  //               src=${beautyProducts[product][brand].src}
-  //               class="card-img-top"
-  //               alt="..."
-  //             />
-  //             <div class="card-body">
-  //               <h5 class="card-title">${beautyProducts[product][brand].name}</h5>
-  //               <p class="card-text">${beautyProducts[product][brand].description}</p>
-  //             </div>
-  //             <div class="card-footer">
-  //               <h5 class="price">Rs.${beautyProducts[product][brand].price}/- </h5>
-  //             </div>
-  //           </div>
-  //         </div>`
-
-  //     }
+        allProducts.innerHTML += createCard(p);
+      }
+    }
+  }
 }
+
+
 function filterProduct() {
   allProducts.innerHTML = "";
-  // console.log(beautyProducts[productMenu.value][brandMenu.value].name);
-  var productData = beautyProducts[productMenu.value][brandMenu.value];
+
+  var category = productMenu.value;
+  var brand = brandMenu.value;
+
+  for (var item in beautyProducts[category][brand]) {
+    var p = beautyProducts[category][brand][item];
+    allProducts.innerHTML += createCard(p);
+  }
+
   allProducts.innerHTML += `
-   
- <div class="col">
-          <div class="card">
-            <img
-              src=${productData.src}
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-title">${productData.name}</h5>
-              <p class="card-text">${productData.description}</p>
-            </div>
-            <div class="card-footer">
-              <h5 class="price">Rs.${productData.price}/- </h5>
-            </div>
-          </div>
-        </div>
-         <div class="col btnCol">
-        <button onclick="viewAll()" class="btn allBtn btn-outline-success" type="button">
-                View All Products
-              </button>
-               </div>
-        `;
+    <div class="col btnCol">
+      <button onclick="viewAll()" class="btn allBtn btn-outline-success">
+        View All Products
+      </button>
+    </div>
+  `;
 }
+
+function createCard(p) {
+  return `
+    <div class="col">
+      <div class="card">
+        <img src="${p.src}" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">${p.Name}</h5>
+          <p class="card-text">${p.Description}</p>
+        </div>
+        <div class="card-footer">
+          <h5 class="price">Rs.${p.price}/-</h5>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+// Load all on first page
+viewAll();
